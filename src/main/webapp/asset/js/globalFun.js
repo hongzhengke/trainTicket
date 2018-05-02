d2cf6c8e1c48b90e13bbe6bcf5a5d1d854731d13
caiwukun <1113765859@qq.com>
2018-05-02 14:44:41 +0800

    caiwukun

---
+++ src/main/webapp/asset/js/globalFun.js
@@ -0,0 +1,44 @@
+function getAjax(method, path, requestParams, requestHeader){
+    return new Promise(function(resolve, reject){
+        var ajax = new XMLHttpRequest();
+        ajax.onreadystatechange = function(){
+            if(ajax.readyState === 4){
+                if(ajax.status >= 200 && ajax.status < 300 || ajax.status === 304){
+                    resolve(this.responseText);
+                }else{
+                    reject(this.responseText);
+                }
+            }
+        }
+        var postData = null;
+        if(method === "get"){
+            let data = ""
+            for(var key in requestParams){
+                if(data === ""){
+                    data += key + "=" + encodeURIComponent(requestParams[key]);
+                }else{
+                    data += "&" + key + "=" + encodeURIComponent(requestParams[key]);
+                }
+            }
+            path += "?" + data;
+        }else if(method === "post"){
+            let data = ""
+            for(var key in requestParams){
+                if(data === ""){
+                    data += key + "=" + requestParams[key];
+                }else{
+                    data += "&" + key + "=" + requestParams[key];
+                }
+            }
+            postData = data;
+        }
+        ajax.open(method, path);
+        for(var key in requestHeader){
+            ajax.setRequestHeader(key, requestHeader[key]);
+        }  //必须在open和send之间调用!
+        ajax.send(postData);
+    });
+}
+function displayMessage(message){
+    alert(message);
+}
--- src/main/webapp/asset/js/login.js
+++ src/main/webapp/asset/js/login.js
@@ -1,7 +1,76 @@
-var login = new Vue({
-    el: "form",
+var vm = new Vue({
+    el: ".form",
     data: {
-        name:
-        password:
+        tel: "",
+        password: "",
+        action: "login",
+        name: "",
+        login_class:{
+            tab_block: true,
+            chosen: true,
+        },
+        signin_class:{
+            tab_block: true,
+            chosen: false,
+        },
+    },
+    methods:{
+        switchTab(event){
+            vm.action = event.target.getAttribute("name");
+        },
+        submit(event){
+            if(vm.action === "login"){
+                let tel = vm.tel.trim();
+                let password = vm.password.trim();
+                if(tel === ""){
+                    displayMessage("电话号码不能为空");
+                }else if(password === ""){
+                    displayMessage("密码不能为空");
+                }else{
+                    let ajax = getAjax("get", "/login.action", {tel,password}, );
+                    ajax.then(
+                        (responseText) => {
+                            displayMessage("登录成功！" + responseText);
+                        },
+                        (responseText) =>{
+                            displayMessage("登录失败！" + responseText);
+                        },
+                    )
+                }
+            }else if(vm.action === "register"){
+                let tel = vm.tel.trim();
+                let userName = vm.userName.trim();
+                let password = vm.password.trim();
+                let ajax = getAjax("get", "/login.action", {tel,name: name,password}, );
+                ajax.then(
+                    (responseText) => {
+                        displayMessage("注册成功！" + responseText);
+                        vm.action = "signin";
+                    },
+                    (responseText) =>{
+                        displayMessage("注册失败！" + responseText);
+                    },
+                )
+            }
+        },
+    },
+    computed:{
+        compute_action(){
+            return this.action === "login" ? "登录" : "注册";
+        },
+        compute_show(){
+            return this.action === "login" ? false : true;
+        }
+    },
+    watch:{
+        action(val){
+            if(val === "login"){
+                vm.login_class.chosen = true;
+                vm.signin_class.chosen = false;
+            }else if(val === "signin"){
+                vm.login_class.chosen = false;
+                vm.signin_class.chosen = true;
+            }
+        }
     }
-})
+});
