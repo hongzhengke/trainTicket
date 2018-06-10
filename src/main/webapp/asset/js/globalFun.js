339c3efd04574064fbb88b65e907ec027176468f
caiwukun <1113765859@qq.com>
2018-06-10 12:30:11 +0800

    界面

--- src/main/webapp/asset/js/globalFun.js
+++ src/main/webapp/asset/js/globalFun.js
@@ -1,52 +1,52 @@
-function getAjax(method, path, requestParams, requestHeader){
-    return new Promise(function(resolve, reject){
-        var ajax = new XMLHttpRequest();
-        ajax.onreadystatechange = function(){
-            if(ajax.readyState === 4){
-                if(ajax.status >= 200 && ajax.status < 300 || ajax.status === 304){
-                    resolve(this.responseText);
-                }else{
-                    reject(this.responseText);
-                }
-            }
+function getAjax(method, path, requestParams, requestHeader) {
+  return new Promise(function(resolve, reject) {
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+      if (ajax.readyState === 4) {
+        if ((ajax.status >= 200 && ajax.status < 300) || ajax.status === 304) {
+          resolve(this.responseText);
+        } else {
+          reject(this.responseText);
         }
-        var postData = null;
-        if(method === "get"){
-            let data = ""
-            for(var key in requestParams){
-                if(data === ""){
-                    data += key + "=" + encodeURIComponent(requestParams[key]);
-                }else{
-                    data += "&" + key + "=" + encodeURIComponent(requestParams[key]);
-                }
-            }
-            path += "?" + data;
-        }else if(method === "post"){
-            let data = ""
-            for(var key in requestParams){
-                if(data === ""){
-                    data += key + "=" + requestParams[key];
-                }else{
-                    data += "&" + key + "=" + requestParams[key];
-                }
-            }
-            postData = data;
+      }
+    };
+    var postData = null;
+    if (method === 'get') {
+      let data = '';
+      for (var key in requestParams) {
+        if (data === '') {
+          data += key + '=' + encodeURIComponent(requestParams[key]);
+        } else {
+          data += '&' + key + '=' + encodeURIComponent(requestParams[key]);
         }
-        ajax.open(method, path);
-        for(var key in requestHeader){
-            ajax.setRequestHeader(key, requestHeader[key]);
-        }  //必须在open和send之间调用!
-        ajax.send(postData);
-    });
+      }
+      path += '?' + data;
+    } else if (method === 'post') {
+      let data = '';
+      for (var key in requestParams) {
+        if (data === '') {
+          data += key + '=' + requestParams[key];
+        } else {
+          data += '&' + key + '=' + requestParams[key];
+        }
+      }
+      postData = data;
+    }
+    ajax.open(method, 'http://172.16.31.101:8080' + path);
+    for (var key in requestHeader) {
+      ajax.setRequestHeader(key, requestHeader[key]);
+    } //必须在open和send之间调用!
+    ajax.send(postData);
+  });
 }
-function displayMessage(message){
-    alert(message);
+function displayMessage(message) {
+  alert(message);
 }
-function displayLoading(){
-    let loading = document.querySelector(".loading");
-    loading.style.visibility = "visible";
+function displayLoading() {
+  let loading = document.querySelector('.loading');
+  loading.style.visibility = 'visible';
 }
-function cancelLoading(){
-    let loading = document.querySelector(".loading");
-    loading.style.visibility = "hidden";
+function cancelLoading() {
+  let loading = document.querySelector('.loading');
+  loading.style.visibility = 'hidden';
 }
--- src/main/webapp/asset/js/main.js
+++ src/main/webapp/asset/js/main.js
@@ -5,22 +5,51 @@ var ticket = {
             <div class="title">余票查询</div>
             <div class="search">
                 <label for="departure">出发地:</label>
-                <select id="departure">
+                <select id="departure" v-model="departure">
                     <option>广州</option>
                     <option>揭阳</option>
                     <option>汕头</option>
                 </select>
                 <label for="destination">目的地:</label>
-                <select id="destination">
+                <select id="destination" v-model="destination">
                     <option>揭阳</option>
-                    <option>广州</option>
                     <option>汕头</option>
+                    <option>台山</option>
+                    <option>普宁</option>
+                    <option>江门</option>
+                    <option>清远</option>
+                    <option>河源</option>
+                    <option>东莞</option>
+                    <option>惠州</option>
+                    <option>佛山</option>
+                    <option>韶关</option>
+                    <option>潮汕</option>
+                    <option>深圳</option>
+                </select>
+                <label for="departureTime">发车时间</label>
+                <select id="departureTime" v-model="departureTime">
+                    <option value="00:00:00">00:00-24:00</option>
+                    <option value="06:00:00">6:00-24:00</option>
+                    <option value="12:00:00">12:00-24:00</option>
+                    <option value="18:00:00">18:00-24:00</option>
                 </select>
                 <div class="dateList">
                     <div v-for="(item, index) in dateList" :class="['dateBlock', selectedIndex==index?'selected':'']" @click="select(index)">
                         {{item.month}} - {{item.date}}
                     </div>
                 </div>
+            </div>
+            <div class="table" v-if="data.length > 0">
+              <div class="table-head">
+                <div v-for="(val, key) in data[0]" :key=key  v-if="key!='ID'" class="th td">{{key}}</div>
+                <div class="th td">订票按钮</div>
+              </div>
+              <div v-for="item in data" :key="item.ID"  class="table-content">
+                <div v-for="(val, key) in item" :key=key v-if="key!='ID'" class="td">{{val}}</div>
+                <div class="td">
+                  <div class="button" @click="buy(item.ID)">购票</div>
+                </div>
+              </div>
             </div>
         </div>
     `,
@@ -30,16 +59,89 @@ var ticket = {
       selectedIndex: 0,
       departure: '广州',
       destination: '揭阳',
-      departureTime: '',
+      departureDate: '',
+      departureTime: '00:00:00',
+      data: [],
+      dict: {
+        departureDate: '出发日期',
+        departureTime: '出发时间',
+        departurePlace: '出发地',
+        destinationPlace: '目的地',
+        code: '编号',
+        duration: '时长',
+        ticketAmount: '剩余票数',
+        carriageAmount: '车厢数',
+      },
     };
   },
   methods: {
     select(index) {
       this.selectedIndex = index;
+      this.departureDate = this.getDepartureDate();
     },
     getDepartureDate() {
       var selectedDate = this.dateList[this.selectedIndex];
       return `${selectedDate.year}-${selectedDate.month}-${selectedDate.date} 00:00:00`;
+    },
+    getData() {
+      var query = {
+        'trainInfo.departurePlace': this.departure,
+        'trainInfo.destinationPlace': this.destination,
+        'ticketInfo.departureDate': this.getDepartureDate(),
+        'trainInfo.departureTime': `0000-00-00 ${this.departureTime}`,
+      };
+      displayLoading();
+      getAjax('get', '/trainTicket/queryTicket.action', query).then(
+        responseText => {
+          var data = JSON.parse(responseText);
+          if (data.message === 'SUCCESS') {
+            if (data.data.length <= 0) {
+              alert('搜索结果为空！');
+              this.data = [];
+            } else {
+              var temp = [];
+              var arr = data.data;
+              arr.forEach(ele => {
+                var object = {};
+                for (var key in this.dict) {
+                  if (ele.ticketInfo[key]) {
+                    object[this.dict[key]] = ele.ticketInfo[key];
+                  } else if (ele.trainInfo[key]) {
+                    object[this.dict[key]] = ele.trainInfo[key];
+                  }
+                }
+                object['出发日期'] = stringifyDate('Y-M-D', new Date(object['出发日期']));
+                object['出发时间'] = stringifyDate('h:m', new Date(object['出发时间']));
+                object['时长'] = parseInt(object['时长'] / 60) + '分钟';
+                object.ID = ele.ticketInfo.id;
+                temp.push(object);
+              });
+              this.data = temp;
+            }
+          } else {
+            alert('加载失败');
+          }
+          cancelLoading();
+        },
+        () => {
+          cancelLoading();
+        }
+      );
+    },
+    buy(id) {
+      displayLoading();
+      getAjax('get', '/trainTicket/buyTicket.action', { 'ticketInfo.id': id }).then(
+        responseText => {
+          var data = JSON.parse(responseText).data;
+          alert(`购票成功! 您的车票座位在${data.carriageNumber}号车厢${data.seatNumber}号座位`);
+          cancelLoading();
+          this.getData();
+        },
+        () => {
+          alert('购票失败!');
+          cancelLoading();
+        }
+      );
     },
   },
   created() {
@@ -53,27 +155,119 @@ var ticket = {
       });
       date.setTime(date.getTime() + inc);
     }
-    getAjax('get', '/trainTicket/queryTicket.action', {
-      'trainInfo.departurePlace': this.departure,
-      'trainInfo.destinationPlace': this.destination,
-      'ticketInfo.departureDate': this.getDepartureDate(),
-      'trainInfo.departureTime': `0000-00-00 00:00:00`,
-    }).then(responseText => {
-      var data = JSON.parse(responseText);
-      if (data.message === 'SUCCESS') {
-        console.log(data.data);
-      }
-    });
+    this.departureDate = this.getDepartureDate();
+  },
+  watch: {
+    loading() {
+      this.getData();
+    },
+  },
+  computed: {
+    loading() {
+      console.log('加载中');
+      return this.departure + this.destination + this.departureDate + this.departureTime;
+    },
   },
 };
 var order = {
   template: `
         <div class="order">
             <div class="title">我的订单</div>
-
-            <div>{{$route.params.id}}</div>
+            <div class="table" v-if="data.length > 0">
+              <div class="table-head">
+                <div v-for="(val, key) in data[0]" :key=key  v-if="key!='ID'" class="th td">{{key}}</div>
+                <div class="th td">订票按钮</div>
+              </div>
+              <div v-for="item in data" :key="item.ID"  class="table-content">
+                <div v-for="(val, key) in item" :key=key v-if="key!='ID'" class="td">{{val}}</div>
+                <div class="td">
+                  <div class="button" @click="refund(item.ID,item['车厢号'],item['座位号'])">退票</div>
+                </div>
+              </div>
+            </div>
         </div>
     `,
+  data() {
+    return {
+      data: [],
+      dict: {
+        departureDate: '出发日期',
+        departureTime: '出发时间',
+        departurePlace: '出发地',
+        destinationPlace: '目的地',
+        code: '编号',
+        duration: '时长',
+        orderTime: '订票时间',
+        carriageNumber: '车厢号',
+        seatNumber: '座位号',
+      },
+    };
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      displayLoading();
+      getAjax('get', '/trainTicket/queryCurrentOrder.action').then(
+        responseText => {
+          var data = JSON.parse(responseText);
+          console.log(data);
+
+          if (data.data.length <= 0) {
+            alert('搜索结果为空！');
+            this.data = [];
+          } else {
+            var temp = [];
+            var arr = data.data;
+            arr.forEach(ele => {
+              var object = {};
+              for (var key in this.dict) {
+                if (ele.ticketInfo[key]) {
+                  object[this.dict[key]] = ele.ticketInfo[key];
+                } else if (ele.trainInfo[key]) {
+                  object[this.dict[key]] = ele.trainInfo[key];
+                } else if (ele.orderInfo[key]) {
+                  object[this.dict[key]] = ele.orderInfo[key];
+                }
+              }
+              object['出发日期'] = stringifyDate('Y-M-D', new Date(object['出发日期']));
+              object['出发时间'] = stringifyDate('h:m', new Date(object['出发时间']));
+              object['订票时间'] = stringifyDate('M-D h:m', new Date(object['订票时间']));
+              object['时长'] = parseInt(object['时长'] / 60) + '分钟';
+              object.ID = ele.ticketInfo.id;
+              temp.push(object);
+            });
+            this.data = temp;
+          }
+          cancelLoading();
+        },
+        () => {
+          alert('查询失败!');
+          cancelLoading();
+        }
+      );
+    },
+    refund(id, cnum, snum) {
+      displayLoading();
+      getAjax('get', '/trainTicket/refundTicket.action', {
+        'ticketInfo.id': id,
+        'orderInfo.carriageNumber': cnum,
+        'orderInfo.seatNumber': snum,
+      }).then(
+        responseText => {
+          console.log(JSON.parse(responseText));
+          alert(`退票成功! `);
+          cancelLoading();
+          this.getData();
+        },
+        () => {
+          alert('退票失败!');
+          cancelLoading();
+        }
+      );
+    },
+  },
 };
 var manage = {
   template: `
